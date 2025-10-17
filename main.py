770aa21bf38f0718a7172ea37a7889817a40144a
r41dy <osinovskijartemij@gmail.com>
2025-10-17 11:48:17 +0500

    unfinished splash anim, fixed splash trigger

--- main.py
+++ main.py
@@ -25,6 +25,17 @@ rain_amount = 5 # more is less
 rain_speed_randomness = 0.25
 horz = 0.5
 
+class Splash():
+    def __init__(self, x, y, vel): # vel is how much right or left the splash should be going (idk)
+        self.x = x
+        self.y = y
+        self.vel = vel
+        self.dots = []
+
+    def anim(self):
+        for _ in range(random.randint(0, 4)):
+            self.dots.append
+
 spawn_interval = random.random()*rain_amount+rain_amount
 
 while run:
@@ -51,8 +62,13 @@ while run:
         pg.draw.aaline(screen, WHITE, vec2(particle_pos.x, particle_pos.y), vec2(particle_pos.x+(horz/(particle_pos.y - prev_pos.y))*rain_length, particle_pos.y-rain_length), rain_width)
         if particle[1] > HEIGHT+rain_length or particle[0]+(horz/(particle_pos.y - prev_pos.y))*rain_length < -1:
             particles.remove(particle)
-        if particle[1] == HEIGHT:
-            print("splash")
+        if particle[1] > HEIGHT and particle[1] < HEIGHT+1:
+            splash = Splash(particle_pos.x, particle_pos.y, 0)
+            splashes.append(splash)
+
+    for splash in splashes:
+        while splash:
+            splash.anim()
 
     pg.display.flip()
 pg.quit
