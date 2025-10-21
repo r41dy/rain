05536af65f8a5e8dbda135e6daf90b86dabc5566
r41dy <osinovskijartemij@gmail.com>
2025-10-21 12:06:39 +0500

    Added framerate independance and color variation

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import random
 
 pg.init()
 WIDTH, HEIGHT = 1920, 1080
-FPS = 60
+FPS = 75
+clock = pg.time.Clock()
+dt = clock.tick(FPS) / 1000.0
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Rain")
 
@@ -20,27 +22,32 @@ splashes = []
 last_spawn_time = pg.time.get_ticks()
 rain_length = 10
 rain_width = 1
-rain_speed = 1.5
-rain_amount = 5 # more is less
+rain_speed = 500
+rain_amount = 100 # more is less
 rain_speed_randomness = 0.25
-horz = 0.5
+horz = 250
+
+#random value (0-255)
+value_a = 180
+value_b = 255
 
 class Splash():
-    def __init__(self, x, y):
+    def __init__(self, x, y, colorvalue):
         self.x = x
         self.y = y
         self.dots = []
+        self.colorvalue = colorvalue
         self.spawn_time = pg.time.get_ticks()
-        for i in range(2, 4):
-            dot = [self.x, self.y-1, (random.random()*2-1), -0.5]
+        for i in range(4):
+            dot = [self.x, self.y-1, (random.random()*2-1), -100]
             self.dots.append(dot)
 
-    def update(self):
+    def update(self, dt):
         for dot in self.dots:
-            dot[0] += dot[2]
-            dot[1] += dot[3]
-            dot[3] += 0.01
-            if dot[1] > HEIGHT+1:
+            dot[0] += dot[2]*100 * dt
+            dot[1] += dot[3] * dt
+            dot[3] += 500 * dt
+            if dot[1] > HEIGHT+5:
                 self.dots.remove(dot)
 
     def debug_draw(self):
@@ -48,7 +55,7 @@ class Splash():
 
     def draw(self):
         for dot in self.dots:
-            pg.draw.circle(screen, WHITE, (dot[0], dot[1]), 1)
+            pg.draw.circle(screen, (self.colorvalue, self.colorvalue, self.colorvalue), (dot[0], dot[1]), 1)
 
     def __del__(self):
         pass
@@ -59,34 +66,35 @@ while run:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+    dt = clock.tick(FPS) / 1000.0
     time = pg.time.get_ticks()
 
     if time - last_spawn_time >= spawn_interval:
         raindrop_x = random.randint(0, int(WIDTH+HEIGHT))
         raindrop_y = 0
         rain_self_speed = rain_speed + (random.random()*rain_speed_randomness-(rain_speed_randomness/2))
-        raindrops.append([raindrop_x, raindrop_y, rain_self_speed])
+        raindrops.append([raindrop_x, raindrop_y, rain_self_speed, random.randint(value_a, value_b), False]) # random color value determination
         spawn_interval = random.random()*rain_amount+rain_amount
         last_spawn_time = time
     
     screen.fill(BLACK)
 
     for raindrop in raindrops:
+        colorvalue = raindrop[3]
         prev_pos = vec2(raindrop[0], raindrop[1])
-        raindrop[1] += raindrop[2]
-        raindrop[0] -= horz
+        raindrop[1] += raindrop[2] * dt
+        raindrop[0] -= horz * dt
         raindrop_pos = vec2(raindrop[0], raindrop[1])
-        splashed = False
-        pg.draw.aaline(screen, WHITE, vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x+(horz/(raindrop_pos.y - prev_pos.y))*rain_length, raindrop_pos.y-rain_length), rain_width)
+        pg.draw.aaline(screen, (colorvalue, colorvalue, colorvalue), vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x+((horz*dt)/(raindrop_pos.y - prev_pos.y))*rain_length, raindrop_pos.y-rain_length), rain_width)
         if raindrop[1] > HEIGHT+rain_length or raindrop[0]+(horz/(raindrop_pos.y - prev_pos.y))*rain_length < -1:
             raindrops.remove(raindrop)
-        if raindrop[1] > HEIGHT and raindrop[1] < HEIGHT+3 and not splashed:
-            splashed = True
-            splash = Splash(raindrop_pos.x, HEIGHT)
+        if raindrop[1] > HEIGHT and not raindrop[4]:
+            raindrop[4] = True
+            splash = Splash(raindrop_pos.x, HEIGHT, colorvalue)
             splashes.append(splash)
 
     for splash in splashes:
-        splash.update()
+        splash.update(dt)
         splash.draw()
 
     pg.display.flip()
