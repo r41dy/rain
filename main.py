fbc52723703f65cd94978554ab2fcc9ff5a36657
r41dy <osinovskijartemij@gmail.com>
2025-10-21 11:07:05 +0500

    Added actual splashes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame as pg
 import random
 
 pg.init()
-WIDTH, HEIGHT = 1280, 720
+WIDTH, HEIGHT = 1920, 1080
 FPS = 60
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Rain")
@@ -31,25 +31,24 @@ class Splash():
         self.y = y
         self.dots = []
         self.spawn_time = pg.time.get_ticks()
+        for i in range(2, 4):
+            dot = [self.x, self.y-1, (random.random()*2-1), -0.5]
+            self.dots.append(dot)
 
-    def anim(self):
-        for i in range(random.randint(0, 4)):
-            self.dots.append([self.x, self.y, ((random.random()*2)-1), random.random()*-1-0.1])
+    def update(self):
         for dot in self.dots:
-            dot_pos = vec2(dot[0], dot[1])
-            dot_y_vel = dot[3]
-            while True:
-                dot_pos.x += dot[2]
-                dot_pos.y += dot_y_vel
-                dot_y_vel += 0.1
-
-    def timer(self):
-        timer = pg.time.get_ticks() - self.spawn_time
-        if timer > 1000:
-            return True
+            dot[0] += dot[2]
+            dot[1] += dot[3]
+            dot[3] += 0.01
+            if dot[1] > HEIGHT+1:
+                self.dots.remove(dot)
 
     def debug_draw(self):
         pg.draw.line(screen, WHITE, vec2(self.x, self.y), vec2(self.x, self.y-10))
+
+    def draw(self):
+        for dot in self.dots:
+            pg.draw.circle(screen, WHITE, (dot[0], dot[1]), 1)
 
     def __del__(self):
         pass
@@ -77,18 +76,18 @@ while run:
         raindrop[1] += raindrop[2]
         raindrop[0] -= horz
         raindrop_pos = vec2(raindrop[0], raindrop[1])
+        splashed = False
         pg.draw.aaline(screen, WHITE, vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x+(horz/(raindrop_pos.y - prev_pos.y))*rain_length, raindrop_pos.y-rain_length), rain_width)
         if raindrop[1] > HEIGHT+rain_length or raindrop[0]+(horz/(raindrop_pos.y - prev_pos.y))*rain_length < -1:
             raindrops.remove(raindrop)
-        if raindrop[1] > HEIGHT and raindrop[1] < HEIGHT+3:
+        if raindrop[1] > HEIGHT and raindrop[1] < HEIGHT+3 and not splashed:
+            splashed = True
             splash = Splash(raindrop_pos.x, HEIGHT)
             splashes.append(splash)
 
     for splash in splashes:
-        splash.debug_draw()
-        if splash.timer():
-            splashes.remove(splash)
-            del splash
+        splash.update()
+        splash.draw()
 
     pg.display.flip()
 pg.quit
