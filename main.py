faaaca914257232fa11745fd609967b4478d3d0d
r41dy <osinovskijartemij@gmail.com>
2025-10-22 12:44:00 +0500

    Improved horizontal velocity

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ RAIN_WIDTH = 1
 RAIN_SPEED = 500
 RAIN_AMOUNT = 5 # less is more
 RAIN_SPEED_RANDOMNESS = 0.25
-HORIZONTAL_RAIN_VELOCITY = 0
+HORIZONTAL_RAIN_VELOCITY = -1000 # negative is left, positive is right
 RAIN_COLOR_R, RAIN_COLOR_G, RAIN_COLOR_B = 255, 255, 255
 value_randomness_factor = 0.25
 
@@ -36,7 +36,8 @@ class Splash():
         self.color = color
         self.spawn_time = pg.time.get_ticks()
         for i in range(4):
-            splash_rotation = vec2((random.random()*2-1)*100, -100).rotate(rotation)
+            if rotation!=0:splash_rotation = vec2((random.random()*2-1)*100, -100).rotate(rotation)
+            else:splash_rotation = vec2((random.random()*2-1)*100+HORIZONTAL_RAIN_VELOCITY/2, -100).rotate(rotation)
             dot = [self.x, self.y-1, splash_rotation]
             self.dots.append(dot)
 
@@ -75,9 +76,9 @@ while run:
     mouse_x, mouse_y = pg.mouse.get_pos()
 
     if time - last_spawn_time >= spawn_interval:
-        if HORIZONTAL_RAIN_VELOCITY > 0:
+        if HORIZONTAL_RAIN_VELOCITY < 0:
             raindrop_x = random.randint(0, int(WIDTH+horizontal_range))
-        elif HORIZONTAL_RAIN_VELOCITY < 0:
+        elif HORIZONTAL_RAIN_VELOCITY > 0:
             raindrop_x = random.randint(-horizontal_range, WIDTH)
         else:
             raindrop_x = random.randint(0, WIDTH)
@@ -95,9 +96,9 @@ while run:
     for raindrop in raindrops:
         prev_pos = vec2(raindrop[0], raindrop[1])
         raindrop[1] += raindrop[2] * dt
-        raindrop[0] -= HORIZONTAL_RAIN_VELOCITY * dt
+        raindrop[0] += HORIZONTAL_RAIN_VELOCITY * dt
         raindrop_pos = vec2(raindrop[0], raindrop[1])
-        pg.draw.aaline(screen, raindrop[4], vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x+((HORIZONTAL_RAIN_VELOCITY*dt)/(raindrop_pos.y - prev_pos.y))*RAIN_LENGTH, raindrop_pos.y-RAIN_LENGTH), RAIN_WIDTH)
+        pg.draw.aaline(screen, raindrop[4], vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x-((HORIZONTAL_RAIN_VELOCITY*dt)/(raindrop_pos.y - prev_pos.y))*RAIN_LENGTH, raindrop_pos.y-RAIN_LENGTH), RAIN_WIDTH)
         if raindrop[1] > HEIGHT and not raindrop[3]:
             raindrop[3] = True
             splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], 0)
@@ -106,7 +107,7 @@ while run:
         if abs(mouse_x - raindrop[0]) < 12 and abs(mouse_y - raindrop[1]) < 19:
             if RAINDROP_MASK.overlap(mouse_mask, (mouse_x - raindrop[0], mouse_y - raindrop[1])) and not raindrop[3]:
                 raindrop[3] = True
-                if HORIZONTAL_RAIN_VELOCITY >= 0 :
+                if HORIZONTAL_RAIN_VELOCITY <= 0 :
                     splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], 45)
                 else:
                     splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], -90)
