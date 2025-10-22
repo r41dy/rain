f9b6ecda92cd73ace115c0d772055de4baac7887
r41dy <osinovskijartemij@gmail.com>
2025-10-22 12:16:32 +0500

    Small improvements (AI)

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ last_spawn_time = pg.time.get_ticks()
 
 # PARAMETERS
 BACKGROUND_COLOR = (0, 0, 0)
-rain_length = 10
-rain_width = 1
-rain_speed = 500
-rain_amount = 5 # less is more
-rain_speed_randomness = 0.25
-horizontal_rain_velocity = 250
-rain_color_R, rain_color_G, rain_color_B = 255, 255, 255
+RAIN_LENGTH = 10
+RAIN_WIDTH = 1
+RAIN_SPEED = 500
+RAIN_AMOUNT = 5 # less is more
+RAIN_SPEED_RANDOMNESS = 0.25
+HORIZONTAL_RAIN_VELOCITY = 0
+RAIN_COLOR_R, RAIN_COLOR_G, RAIN_COLOR_B = 255, 255, 255
 value_randomness_factor = 0.25
 
 class Splash():
@@ -45,8 +45,7 @@ class Splash():
             dot[0] += dot[2].x * dt
             dot[1] += dot[2].y * dt
             dot[2].y += 500 * dt
-            if dot[1] > HEIGHT+5:
-                self.dots.remove(dot)
+        self.dots = [dot for dot in self.dots if dot[1] <= HEIGHT + 5]
 
     def draw(self):
         for dot in self.dots:
@@ -55,7 +54,7 @@ class Splash():
     def __del__(self):
         pass
 
-spawn_interval = random.random()*rain_amount+rain_amount
+spawn_interval = random.random()*RAIN_AMOUNT+RAIN_AMOUNT
 
 pg.mouse.set_visible(False)
 
@@ -63,7 +62,11 @@ mouse_image = pg.image.load("mouse.png").convert_alpha()
 mouse_image_rect = mouse_image.get_rect()
 mouse_mask = pg.mask.from_surface(mouse_image)
 
+RAINDROP_MASK = pg.mask.from_surface(pg.Surface((1, 1)))
+
 while run:
+    fall_time = HEIGHT / (RAIN_SPEED*dt)
+    horizontal_range = int(abs(HORIZONTAL_RAIN_VELOCITY*dt) * fall_time)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
@@ -72,37 +75,44 @@ while run:
     mouse_x, mouse_y = pg.mouse.get_pos()
 
     if time - last_spawn_time >= spawn_interval:
-        raindrop_x = random.randint(0, int(WIDTH+HEIGHT))
+        if HORIZONTAL_RAIN_VELOCITY > 0:
+            raindrop_x = random.randint(0, int(WIDTH+horizontal_range))
+        elif HORIZONTAL_RAIN_VELOCITY < 0:
+            raindrop_x = random.randint(-horizontal_range, WIDTH)
+        else:
+            raindrop_x = random.randint(0, WIDTH)
         raindrop_y = 0
         rain_value_randomness = random.random()*value_randomness_factor
-        rain_color = (rain_color_R-rain_color_R*rain_value_randomness, rain_color_G-rain_color_G*rain_value_randomness, rain_color_B-rain_color_B*rain_value_randomness)
-        rain_self_speed = rain_speed + (random.random()*rain_speed_randomness-(rain_speed_randomness/2))
+        rain_color = (RAIN_COLOR_R-RAIN_COLOR_R*rain_value_randomness, RAIN_COLOR_G-RAIN_COLOR_G*rain_value_randomness, RAIN_COLOR_B-RAIN_COLOR_B*rain_value_randomness)
+        rain_self_speed = RAIN_SPEED + (random.random()*RAIN_SPEED_RANDOMNESS-(RAIN_SPEED_RANDOMNESS/2))
         raindrops.append([raindrop_x, raindrop_y, rain_self_speed, False, rain_color]) # random color value determination
-        spawn_interval = random.random()*rain_amount+rain_amount
+        spawn_interval = random.random()*RAIN_AMOUNT+RAIN_AMOUNT
         last_spawn_time = time
     
     screen.fill(BACKGROUND_COLOR)
 
+    raindrops_to_remove = []
     for raindrop in raindrops:
-        raindrop_mask = pg.mask.from_surface(pg.Surface((1, 1)))
         prev_pos = vec2(raindrop[0], raindrop[1])
         raindrop[1] += raindrop[2] * dt
-        raindrop[0] -= horizontal_rain_velocity * dt
+        raindrop[0] -= HORIZONTAL_RAIN_VELOCITY * dt
         raindrop_pos = vec2(raindrop[0], raindrop[1])
-        pg.draw.aaline(screen, raindrop[4], vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x+((horizontal_rain_velocity*dt)/(raindrop_pos.y - prev_pos.y))*rain_length, raindrop_pos.y-rain_length), rain_width)
+        pg.draw.aaline(screen, raindrop[4], vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x+((HORIZONTAL_RAIN_VELOCITY*dt)/(raindrop_pos.y - prev_pos.y))*RAIN_LENGTH, raindrop_pos.y-RAIN_LENGTH), RAIN_WIDTH)
         if raindrop[1] > HEIGHT and not raindrop[3]:
             raindrop[3] = True
             splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], 0)
             splashes.append(splash)
-            raindrops.remove(raindrop)
-        if raindrop_mask.overlap(mouse_mask, (mouse_x - raindrop_pos.x, mouse_y - raindrop_pos.y)) and not raindrop[3]:
-            raindrop[3] = True
-            if horizontal_rain_velocity > 0:
-                splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], 45)
-            else:
-                splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], -90)
-            splashes.append(splash)
-            raindrops.remove(raindrop)
+            raindrops_to_remove.append(raindrop)
+        if abs(mouse_x - raindrop[0]) < 12 and abs(mouse_y - raindrop[1]) < 19:
+            if RAINDROP_MASK.overlap(mouse_mask, (mouse_x - raindrop[0], mouse_y - raindrop[1])) and not raindrop[3]:
+                raindrop[3] = True
+                if HORIZONTAL_RAIN_VELOCITY >= 0 :
+                    splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], 45)
+                else:
+                    splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], -90)
+                splashes.append(splash)
+                raindrops_to_remove.append(raindrop)
+    raindrops = [r for r in raindrops if r not in raindrops_to_remove]
 
     for splash in splashes:
         splash.update(dt)
