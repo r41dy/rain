f56a20a1d67ad29708236968f283d58159cf3d87
r41dy <osinovskijartemij@gmail.com>
2025-10-29 12:46:33 +0500

    Small improvements

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pg.init()
 WIDTH, HEIGHT = 1920, 1080
 FPS = 60
 clock = pg.time.Clock()
-dt = clock.tick(FPS) / 1000.0
+dt = 1.0 / FPS
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Rain")
 
@@ -22,41 +22,45 @@ BACKGROUND_COLOR = (0, 0, 0)
 RAIN_LENGTH = 10
 RAIN_WIDTH = 1
 RAIN_SPEED = 500
-RAIN_AMOUNT = 5 # less is more
+RAIN_AMOUNT = 50 # less is more
 RAIN_SPEED_RANDOMNESS = 0.25
 HORIZONTAL_RAIN_VELOCITY = -250 # negative is left, positive is right
 RAIN_COLOR_R, RAIN_COLOR_G, RAIN_COLOR_B = 255, 255, 255
-color_by_depth_factor = 0.25
-uhhhh_idk = 0.98
+color_by_depth_factor = 0.5
+uhhhh_idk = 0.8
 DIM_REFLECTION = 0.5
-ENABLE_REFLECTIONS = True
+ENABLE_REFLECTIONS = False
 
 class Splash():
-    def __init__(self, x, y, color, rotation, depth, do_spawn_wave):
+    def __init__(self, x, y, R, G, B, rotation, depth, do_spawn_wave):
         self.x = x
         self.y = y
         self.dots = []
-        self.color = color
+        self.R = R
+        self.G = G
+        self.B = B
         self.spawn_time = pg.time.get_ticks()
         self.depth = depth
         self.do_spawn_wave = do_spawn_wave
         if self.do_spawn_wave:
             self.waves = []
-            self.waves.append([self.x, self.y, 0, 0, 0.25, 100*dt, 0, 50, self.color])
-        self.depth_for_color = 1-(self.depth*color_by_depth_factor)
+            self.waves.append([self.x, self.y, 0, 0, 0.25, random.randint(70, 130), 0, 50, self.R, self.G, self.B])
+        self.depth_for_color = depth*color_by_depth_factor
         for i in range(random.randint(2, 4)):
             if rotation!=0:splash_rotation = vec2((random.random()*2-1)*50, -100).rotate(rotation)
             else:splash_rotation = vec2((random.random()*2-1)*100+HORIZONTAL_RAIN_VELOCITY/2, -100).rotate(rotation)
-            dot = [self.x, self.y-2, splash_rotation]
+            dot = [self.x, self.y-2, splash_rotation, 0]
             self.dots.append(dot)
 
     def update(self, dt):
         if self.do_spawn_wave:
             for wave in self.waves:
-                wave[2] += wave[5]
+                wave[2] += wave[5] * dt
                 wave[3] = wave[2]*wave[4]
                 wave[6] += 1
-                wave[8] = (RAIN_COLOR_R*self.depth_for_color*(1-(wave[6]/wave[7])), RAIN_COLOR_G*self.depth_for_color*(1-(wave[6]/wave[7])), RAIN_COLOR_B*self.depth_for_color*(1-(wave[6]/wave[7])))
+                wave[8] = self.R*(1-(wave[6]/wave[7]))
+                wave[9] = self.G*(1-(wave[6]/wave[7]))
+                wave[10] = self.B*(1-(wave[6]/wave[7]))
                 if wave[6] > wave[7]:
                     self.waves.remove(wave)
 
@@ -64,14 +68,15 @@ class Splash():
             dot[0] += dot[2].x * dt
             dot[1] += dot[2].y * dt
             dot[2].y += 500 * dt
-        self.dots = [dot for dot in self.dots if dot[1] <= HEIGHT*(depth*(1-uhhhh_idk)+uhhhh_idk)]
+            dot[3] += 1
+        self.dots = [dot for dot in self.dots if dot[1] <= HEIGHT*(depth*(1-uhhhh_idk)+uhhhh_idk) or dot[3] < 20]
 
     def draw(self):
         if self.do_spawn_wave:
             for wave in self.waves:
-                pg.draw.ellipse(screen, wave[8], (wave[0]-wave[2]/2, wave[1]-wave[3]/2, wave[2], wave[3]), 1)
+                pg.draw.ellipse(screen, (wave[8], wave[9], wave[10]), (wave[0]-wave[2]/2, wave[1]-wave[3]/2, wave[2], wave[3]), 1)
         for dot in self.dots:
-            pg.draw.circle(screen, self.color, (dot[0], dot[1]), 1)
+            pg.draw.circle(screen, (self.R, self.G, self.B), (dot[0], dot[1]), 1)
 
 #        if ENABLE_REFLECTIONS:
 #            if self.do_spawn_wave:
@@ -84,7 +89,7 @@ class Splash():
     def __del__(self):
         pass
 
-spawn_interval = random.random()*RAIN_AMOUNT+RAIN_AMOUNT
+spawn_interval = RAIN_AMOUNT + random.uniform(-RAIN_AMOUNT/2, RAIN_AMOUNT/2)
 
 pg.mouse.set_visible(False)
 
@@ -112,12 +117,14 @@ while run:
         else:
             raindrop_x = random.randint(0, WIDTH)
         depth = random.random()
-        depth_for_color = depth*color_by_depth_factor
+        depth_for_color = (1-depth)*color_by_depth_factor
         raindrop_y = 0
-        rain_color = (RAIN_COLOR_R-RAIN_COLOR_R*depth_for_color, RAIN_COLOR_G-RAIN_COLOR_G*depth_for_color, RAIN_COLOR_B-RAIN_COLOR_B*depth_for_color)
+        raindrop_R = RAIN_COLOR_R-RAIN_COLOR_R*depth_for_color
+        raindrop_G = RAIN_COLOR_G-RAIN_COLOR_G*depth_for_color
+        raindrop_B = RAIN_COLOR_B-RAIN_COLOR_B*depth_for_color
         rain_self_speed = RAIN_SPEED + (random.random()*RAIN_SPEED_RANDOMNESS-(RAIN_SPEED_RANDOMNESS/2))
-        raindrops.append([raindrop_x, raindrop_y, rain_self_speed, False, rain_color, depth]) # random color value determination
-        spawn_interval = random.random()*RAIN_AMOUNT+RAIN_AMOUNT
+        raindrops.append([raindrop_x, raindrop_y, rain_self_speed, False, raindrop_R, raindrop_G, raindrop_B, depth]) # random color value determination
+        spawn_interval = RAIN_AMOUNT + random.uniform(-RAIN_AMOUNT/2, RAIN_AMOUNT/2)
         last_spawn_time = time
     
     screen.fill(BACKGROUND_COLOR)
@@ -128,19 +135,22 @@ while run:
         raindrop[1] += raindrop[2] * dt
         raindrop[0] += HORIZONTAL_RAIN_VELOCITY * dt
         raindrop_pos = vec2(raindrop[0], raindrop[1])
-        pg.draw.aaline(screen, raindrop[4], vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x-((HORIZONTAL_RAIN_VELOCITY*dt)/(raindrop_pos.y - prev_pos.y))*RAIN_LENGTH, raindrop_pos.y-RAIN_LENGTH), RAIN_WIDTH)
-        if raindrop[1] > HEIGHT*(raindrop[5]*(1-uhhhh_idk)+uhhhh_idk) and not raindrop[3]:
+        if ENABLE_REFLECTIONS:
+            reflection_y = HEIGHT - (raindrop_pos.y - HEIGHT*uhhhh_idk)
+            pg.draw.aaline(screen, (raindrop[4]*DIM_REFLECTION, raindrop[5]*DIM_REFLECTION, raindrop[6]*DIM_REFLECTION), vec2(raindrop_pos.x, reflection_y), vec2(raindrop_pos.x-((HORIZONTAL_RAIN_VELOCITY*dt)/(raindrop_pos.y - prev_pos.y))*RAIN_LENGTH, reflection_y+RAIN_LENGTH), RAIN_WIDTH)
+        pg.draw.aaline(screen, (raindrop[4], raindrop[5], raindrop[6]), vec2(raindrop_pos.x, raindrop_pos.y), vec2(raindrop_pos.x-((HORIZONTAL_RAIN_VELOCITY*dt)/(raindrop_pos.y - prev_pos.y))*RAIN_LENGTH, raindrop_pos.y-RAIN_LENGTH), RAIN_WIDTH)
+        if raindrop[1] > HEIGHT*(raindrop[7]*(1-uhhhh_idk)+uhhhh_idk) and not raindrop[3]:
             raindrop[3] = True
-            splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], 0, raindrop[5], True)
+            splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], raindrop[5], raindrop[6], 0, raindrop[5], True)
             splashes.append(splash)
             raindrops_to_remove.append(raindrop)
         if abs(mouse_x - raindrop[0]) < 12 and abs(mouse_y - raindrop[1]) < 19:
             if RAINDROP_MASK.overlap(mouse_mask, (mouse_x - raindrop[0], mouse_y - raindrop[1])) and not raindrop[3]:
                 raindrop[3] = True
                 if HORIZONTAL_RAIN_VELOCITY <= 0 :
-                    splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], 45, raindrop[5], False)
+                    splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], raindrop[5], raindrop[6], 45, raindrop[5], False)
                 else:
-                    splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], -90, raindrop[5], False)
+                    splash = Splash(raindrop_pos.x, raindrop_pos.y, raindrop[4], raindrop[5], raindrop[6], -90, raindrop[5], False)
                 splashes.append(splash)
                 raindrops_to_remove.append(raindrop)
     raindrops = [r for r in raindrops if r not in raindrops_to_remove]
@@ -148,6 +158,7 @@ while run:
     for splash in splashes:
         splash.update(dt)
         splash.draw()
+    splashes = [s for s in splashes if s.dots or (s.do_spawn_wave and s.waves)]
 
     screen.blit(mouse_image, (mouse_x, mouse_y))
 
